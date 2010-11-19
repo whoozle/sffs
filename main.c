da04289a443656749c4e9a34ce6c5cb1f19d41be
Vladimir <whoozle@yandex.ru>
2010-11-19 16:25:48 +0300

    added "test" command

--- main.c
+++ main.c
@@ -183,6 +183,31 @@ int main(int argc, char **argv) {
 		}
 
 		sffs_umount(&fs);
+	} else if (strcmp(argv[2], "test") == 0) {
+		char i, buf[120];
+		for(i = 0; i < 120; ++i) 
+			buf[i] = i;
+		if (mount_image(&fs, argv[1]) == -1)
+			return 2;
+
+		sffs_write(&fs, "f1", buf, 4);
+		sffs_write(&fs, "f2", buf, 4);
+		sffs_write(&fs, "f3", buf, 4);
+		sffs_write(&fs, "f1", buf, 4);
+		/*
+		sffs_write(&fs, "f2", buf, 4);
+		sffs_write(&fs, "f3", buf, 30);
+		sffs_write(&fs, "f1", buf, 4);
+		sffs_write(&fs, "f2", buf, 4);
+		sffs_write(&fs, "f3", buf, 60);
+		sffs_write(&fs, "f1", buf, 4);
+		sffs_write(&fs, "f2", buf, 4);
+		sffs_write(&fs, "f3", buf, 90);
+		sffs_write(&fs, "f1", buf, 4);
+		sffs_write(&fs, "f2", buf, 4);
+		sffs_write(&fs, "f3", buf, 120);
+		*/
+		sffs_umount(&fs);
 	} else {
 		printf("unknown command: %s\n", argv[2]);
 	}
