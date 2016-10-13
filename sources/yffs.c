f24a345bb36a37b313042d2f984a440968a97efc
Wyatt Dahlenburg <wdahlenburg@users.noreply.github.com>
2016-10-13 17:01:16 -0400

    Update yffs.c
    Merge conflict

--- sources/yffs.c
+++ sources/yffs.c
@@ -203,11 +203,8 @@ static int yffs_compact(struct yffs *fs) {
 	size_t i, n = fs->free.size / sizeof(struct yffs_block);
 	if (n < 2)
 		return 0;
-<<<<<<< HEAD
+
 	//LOG_ERROR(("yffs: compacting free space..."));
-=======
-	//LOG_DEBUG(("yffs: compacting free space..."));
->>>>>>> origin/Brian
 	--n;
 	for(i = 0; i < n; ) {
 		size_t j = i + 1;
@@ -259,11 +256,9 @@ static int yffs_recover_and_remove_old_files(struct yffs *fs) {
 static int yffs_unlink_at(struct yffs *fs, size_t pos) {
 	struct yffs_entry *file = ((struct yffs_entry *)fs->files.ptr) + pos;
 	struct yffs_block *free_block;
-<<<<<<< HEAD
-	//LOG_ERROR(("yffs: erasing metadata[%zu]:%s at 0x%zx-0x%zx", pos, file->name, file->block.begin, file->block.end));
-=======
+
 	// LOG_DEBUG(("yffs: erasing metadata[%zu]:%s at 0x%zx-0x%zx", pos, file->name, file->block.begin, file->block.end));
->>>>>>> origin/Brian
+
 	file->block.mtime = (uint32_t)time(0);
 	if (yffs_write_metadata(fs, &file->block, 0, 0, 0) == -1)
 		return -1;
@@ -546,11 +541,9 @@ int yffs_mount(struct yffs *fs) {
 			}
 			file->size = block_size - filename_len - padding;
 			file->name[filename_len] = 0;
-<<<<<<< HEAD
-			//LOG_ERROR(("yffs: read file %s -> %zu", file->name, file->size));
-=======
+
 			// LOG_DEBUG(("yffs: read file %s -> %zu", file->name, file->size));
->>>>>>> origin/Brian
+
 			file->block = block;
 		} else {
 			struct yffs_block *free;
@@ -559,11 +552,9 @@ int yffs_mount(struct yffs *fs) {
 				goto error;
 			free = (struct yffs_block *)((char *)fs->free.ptr + free_offset);
 			*free = block;
-<<<<<<< HEAD
-			//LOG_ERROR(("yffs: free space %zu->%zu", block.begin, block.end));
-=======
+
 			// LOG_DEBUG(("yffs: free space %zu->%zu", block.begin, block.end));
->>>>>>> origin/Brian
+
 			if (free->end > fs->device_size)  {
 				//LOG_ERROR(("yffs: free spaces crosses device bound!"));
 				free->end = fs->device_size;
@@ -595,11 +586,8 @@ int yffs_mount(struct yffs *fs) {
 	if (yffs_compact(fs) == -1)
 		goto error;
 	
-<<<<<<< HEAD
-	//LOG_ERROR(("yffs: mounted!"));
-=======
 	// LOG_DEBUG(("yffs: mounted!"));
->>>>>>> origin/Brian
+
 	return 0;
 	
 error:
