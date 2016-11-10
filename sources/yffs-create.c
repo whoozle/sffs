6e8e4d5134f3fc38e7070a03e16ba10c69de50a3
Samuel Spencer <samuel@spencer.vision>
2016-11-10 15:44:17 -0500

    Update yffs-create.c
    header includes used header flag comment

--- sources/yffs-create.c
+++ sources/yffs-create.c
@@ -18,7 +18,7 @@ static int yffs_write_empty_header(struct yffs *fs, struct yffs_block *block) {
         LOG_ERROR(("yffs: avoid writing block <= 16b."));
         return -1;
     }
-    char header[yffs_HEADER_SIZE] = {
+    char header[yffs_HEADER_SIZE] = { //'header' indexes used: 0, 1, 10, 12, 13, 14, 15
         0, 0, 0, 0, 0, /*1:flags(empty) + size, current 1st*/
         0, 0, 0, 0, 0, /*2:flags + size*/
         0, 0, 0, 0, 0, 0, /*mtime + padding + fname len */
