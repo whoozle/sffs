9751adca478c69cd492cc9169d7c12467cba6352
Ztkent <ztkent@gmail.com>
2016-11-28 19:26:26 -0500

    More demo changes

--- project_resources/demo/demo3.sh
+++ project_resources/demo/demo3.sh
@@ -8,14 +8,14 @@ printf "\n"
 
 read -p "[Enter]" var
 if [ ${#var} -eq 0 ]; then
-  ../../bin/yffs-create iso.img 500000
+	echo "Creating a file system: yffs-create iso.img 10000";
+	../../bin/yffs-create iso.img 500000
 fi
 
-printf "\nInserting files into image\n"
 
 read -p "[Enter]" var
 if [ ${#var} -eq 0 ]; then
-  echo "Creating files"
+  echo "Inserting files into image: yffs-add iso.img Filename";
   for i in {0..25}
     do
      echo "File${i}" > "File${i}.txt"
@@ -23,29 +23,28 @@ if [ ${#var} -eq 0 ]; then
     done  
 fi
 
-printf "\nListing Filesystem Contents\n"
-
 read -p "[Enter]" var
 if [ ${#var} -eq 0 ]; then 
+  echo "Listing files in image: yffs-ls iso.img";
   ../../bin/yffs-ls iso.img
 fi
 
-printf "\nRemoving odds from image\n"
 
 read -p "[Enter]" var
 if [ ${#var} -eq 0 ]; then
-  for i in {0..25}
-    do
-      if [ $((i%2)) -ne 0 ]; then
-	../../bin/yffs-rm iso.img "File${i}.txt"
-      fi
-    done
+	echo "Removing odd files in image: yffs-rm iso.img FileX";
+	for i in {0..25}; do
+    	  if [ $((i%2)) -ne 0 ]; then
+		../../bin/yffs-rm iso.img "File${i}.txt"
+      	  fi
+    	done
 fi
 
 printf "\nReading all files divisible by 10\n"
 
 read -p "[Enter]" var
 if [ ${#var} -eq 0 ]; then
+  echo "Reading all files divisible by 10: yffs-cat iso.img FileX ";
   for i in {0..25}
     do
       if [ $((i%10)) -eq 0 ]; then
@@ -54,18 +53,17 @@ if [ ${#var} -eq 0 ]; then
     done
 fi
 
-printf "\nUpdating the first 5 files\n"
 
 read -p "[Enter]" var
 if [ ${#var} -eq 0 ]; then
+  echo "Updating the first 5 files: yffs-edit iso.img FileX " New Text "";
   for i in {0..10}
     do
       #We have to take the even files because we removed the odds 
       if [ $((i%2)) -eq 0 ]; then
 	python -c 'print "*"*25'
-        echo "Contents File${i}.txt: "
-        echo "Mod Text ${i} " > toAppend;
-          ../../bin/yffs-edit iso.img "File${i}.txt" "toAppend"
+        echo "New Text for File${i} " > toAppend;
+        ../../bin/yffs-edit iso.img "File${i}.txt" "toAppend"
 	rm "toAppend"
 	../../bin/yffs-cat iso.img "File${i}.txt"
       fi
@@ -73,7 +71,7 @@ if [ ${#var} -eq 0 ]; then
 fi
 
 python -c 'print "*"*25'
-printf "Creating Test Directorys\n"
+printf "Creating Test Directorys\n yffs-add iso.img \"TestFileA\" \"FolderA\" -1\n yffs-add iso.img \"TestFileB\" \"FolderB\" ";
 python -c 'print "*"*25'
 mkdir FolderA
 mkdir FolderB
@@ -84,53 +82,35 @@ echo "Content2" > FolderB/TestFileB
 echo "Content3" > FolderB/TestFileC
 echo "Content4" > FolderB/TestFileD
 
+
 ../../bin/yffs-add iso.img "FolderA/TestFileA" -1 > /dev/null
 ../../bin/yffs-add iso.img "FolderB/TestFileB" > /dev/null
 
-printf "\nListing Filesystem Content "
 read -p "[Enter]"
 if [ ${#var} -eq 0 ]; then
+  echo "Listing files in image: yffs-ls iso.img";
   ../../bin/yffs-ls iso.img
 fi
 
 printf "\nEditing File Owners "
 read -p "[Enter]" var
 if [ ${#var} -eq 0 ]; then
-#CHOWN	
-	echo;
-	python -c 'print "*"*25'
-	printf "Before CHOWN\n"
-	python -c 'print "*"*25'
-
-  ../../bin/yffs-ls iso.img | head -n4 | tail -n4; echo; echo;
+  echo "Editing File Owners in image: yffs-chown iso.img File0.txt";
+	#CHOWN	
   ../../bin/yffs-chown iso.img File0.txt UserA
   ../../bin/yffs-chown iso.img File10.txt UserB
-	
-	python -c 'print "*"*25'
-	printf "After CHOWN\n"
-	python -c 'print "*"*25'
-  
   ../../bin/yffs-ls iso.img | head -n4 | tail -n4; echo; echo;
 fi
+
 printf "\nEditing File Permissions "
 read -p "[Enter]" var
 if [ ${#var} -eq 0 ]; then
 #CHMOD
-	echo;
-	python -c 'print "*"*25'
-	printf "Before CHMOD\n"
-	python -c 'print "*"*25'  
-
-  ../../bin/yffs-ls iso.img | tail -n5; echo; echo;
+  echo "Editing File Permissions in image: yffs-chown iso.img File0.txt";
   ../../bin/yffs-chmod iso.img File4.txt 10
   ../../bin/yffs-chmod iso.img File6.txt 10
   ../../bin/yffs-chmod iso.img File8.txt 10
   ../../bin/yffs-chmod iso.img GpmefsB0UftuGjmfB 10
-	
-	python -c 'print "*"*25'
-	printf "After CHMOD\n"
-	python -c 'print "*"*25'
-  
   ../../bin/yffs-ls iso.img | tail -n5; echo; echo;
 fi
 
@@ -149,25 +129,15 @@ printf "Printing Folder Contents\n"
 python -c 'print "*"*25'
 read -p "[Enter]"
 if [ ${#var} -eq 0 ]; then
-  #../../bin/yffs-ls iso.img FolderB; echo; echo;
   echo -n "GpmefsB0UftuGjmfB -> ";ls Folder*
 fi
 
-read -p "[Enter]" var
-if [ ${#var} -eq 0 ]; then
-#Showing fs	
-	echo;
-	hexdump -C iso.img > fsContents;
-	xxd -b iso.img > ffsContents;
-	cat fsContents;
-fi
-
-	echo;echo;
-	python -c 'print "*"*25'
-	printf "Demo completed\n"
-	python -c 'print "*"*25'
-	echo;
-	read -p "[Enter to clean and end]"
-	rm iso.img
-	rm File* f*sContents;
-	rm -rf *Folder*
+echo;echo;
+python -c 'print "*"*25'
+printf "Demo completed\n"
+python -c 'print "*"*25'
+echo;
+read -p "[Enter to clean and end]"
+rm iso.img
+rm File*;
+rm -rf *Folder*
